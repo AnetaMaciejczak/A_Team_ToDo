b5b2f145e444da12b15ba4eec3eb7ed1e428e952
Your Name <you@example.com>
2018-06-29 14:00:55 +0200

    Znikajacy opis

--- js/app.js
+++ js/app.js
@@ -3,279 +3,289 @@ document.addEventListener("DOMContentLoaded", function () {
     /*łapię button dodający zadanie*/
     var add = document.querySelector("#main-form-btn-add");
 
-        var tasks = [];
-        var counter = 1;
+    var tasks = [];
+    var counter = 1;
 
-        /*łapię listę do której będziemy dodawać li*/
-        var taskList = document.querySelector(".main-tusks-list");
+    /*łapię listę do której będziemy dodawać li*/
+    var taskList = document.querySelector(".main-tusks-list");
 
-        function readLocalStorage() {
-            var tasks = JSON.parse(localStorage.getItem('tasks'));
-            // console.log(tasks);
-            if (!tasks) {
-                tasks = [];
-            }
-            for (var i = 0; i < tasks.length; i++) {
-                createElement(tasks[i]);
-            }
-
+    function readLocalStorage() {
+        var tasks = JSON.parse(localStorage.getItem('tasks'));
+        // console.log(tasks);
+        if (!tasks) {
+            tasks = [];
+        }
+        for (var i = 0; i < tasks.length; i++) {
+            createElement(tasks[i]);
         }
 
-        function createElement(taskObj) {
+    }
 
-            console.log(taskObj);
-            /*tworzę nowe li i poszczególne elementy*/
-            /*obiekt li*/
-            var newTaskLi = document.createElement("li");
-            var newTaskLiDown = document.createElement("li");
-            var newTaskBtnComplete = document.createElement("input");
-            newTaskBtnComplete.setAttribute("type", "checkbox");
-            var newTaskTitle = document.createElement("span");
-            var newTaskDate = document.createElement("span");
-            var newTaskPriority = document.createElement("span");
-            var newTaskDescription = document.createElement("p");
-            var newTaskDBtnDeleted = document.createElement("span");
+    function createElement(taskObj) {
 
-            /*dodaję nowym elementom klasy, aby można je było łatwo stylować*/
-            newTaskLi.classList.add("new-task-li");
-            newTaskLiDown.classList.add("new-task-li-down");
-            newTaskBtnComplete.classList.add("new-task-btn-completed");
-            newTaskTitle.classList.add("new-task-title");
-            newTaskDate.classList.add("new-task-date");
-            newTaskPriority.classList.add("new-task-priority");
-            newTaskDescription.classList.add("new-task-description");
-            newTaskDBtnDeleted.classList.add("new-task-btn-deleted");
+        console.log(taskObj);
+        /*tworzę nowe li i poszczególne elementy*/
+        /*obiekt li*/
+        var newTaskLi = document.createElement("li");
+        var newTaskLiDown = document.createElement("li");
+        var newTaskBtnComplete = document.createElement("input");
+        newTaskBtnComplete.setAttribute("type", "checkbox");
+        var newTaskTitle = document.createElement("span");
+        var newTaskDate = document.createElement("span");
+        var newTaskPriority = document.createElement("span");
+        var newTaskDescription = document.createElement("p");
+        var newTaskDBtnDeleted = document.createElement("span");
 
-            /*dodaję teksty wewnętrzne do poszczególnych elentów*/
-            newTaskTitle.innerText = taskObj.title;
-            newTaskDate.innerText = taskObj.date;
-            newTaskPriority.innerText = taskObj.priority;
-            newTaskDescription.innerText = taskObj.description;
-            newTaskDBtnDeleted.innerText = "";
+        /*dodaję nowym elementom klasy, aby można je było łatwo stylować*/
+        newTaskLi.classList.add("new-task-li");
+        newTaskLiDown.classList.add("new-task-li-down");
+        newTaskBtnComplete.classList.add("new-task-btn-completed");
+        newTaskTitle.classList.add("new-task-title");
+        newTaskDate.classList.add("new-task-date");
+        newTaskPriority.classList.add("new-task-priority");
+        newTaskDescription.classList.add("new-task-description");
+        newTaskDBtnDeleted.classList.add("new-task-btn-deleted");
 
-            newTaskLi.appendChild(newTaskBtnComplete);
-            newTaskLi.appendChild(newTaskTitle);
-            newTaskLi.appendChild(newTaskDate);
-            newTaskLi.appendChild(newTaskPriority);
-            // newTaskLi.appendChild(newTaskDescription);
-            newTaskLi.appendChild(newTaskDBtnDeleted);
+        /*dodaję teksty wewnętrzne do poszczególnych elentów*/
+        newTaskTitle.innerText = taskObj.title;
+        newTaskDate.innerText = taskObj.date;
+        newTaskPriority.innerText = taskObj.priority;
+        newTaskDescription.innerText = taskObj.description;
+        newTaskDBtnDeleted.innerText = "";
 
-            newTaskLiDown.appendChild(newTaskDescription);
+        newTaskLi.appendChild(newTaskBtnComplete);
+        newTaskLi.appendChild(newTaskTitle);
+        newTaskLi.appendChild(newTaskDate);
+        newTaskLi.appendChild(newTaskPriority);
+        // newTaskLi.appendChild(newTaskDescription);
+        newTaskLi.appendChild(newTaskDBtnDeleted);
 
-            /*dodaję nowe li do listy zadań*/
-            taskList.appendChild(newTaskLi);
-            console.log(newTaskLi);
+        newTaskLiDown.appendChild(newTaskDescription);
 
-            taskList.appendChild(newTaskLiDown);
+        /*dodaję nowe li do listy zadań*/
+        taskList.appendChild(newTaskLi);
+        console.log(newTaskLi);
 
-            /*dodaję do buttona deleted event*/
-            newTaskDBtnDeleted.addEventListener("click", function () {
-                taskList.removeChild(newTaskLi);
-                taskList.removeChild(newTaskLiDown);
-                /*dodać removowe local storage*/
-            });
+        taskList.appendChild(newTaskLiDown);
+
+        /*dodaję do buttona deleted event*/
+        newTaskDBtnDeleted.addEventListener("click", function () {
+            taskList.removeChild(newTaskLi);
+            taskList.removeChild(newTaskLiDown);
+            /*dodać removowe local storage*/
+        });
+    }
+
+    readLocalStorage();
+
+
+    console.log(localStorage.getItem('tasks'));
+
+    /*nadaję event na button dodający zadanie*/
+    add.addEventListener("click", function () {
+
+        /*łapię wszystkie elementy*/
+        var title = document.querySelector("#title");
+        var date = document.querySelector("#date");
+        var priority = document.querySelector("#priority");
+        var description = document.querySelector("#description");
+
+        /* walidacja danych */
+        var errorMessage = document.querySelector(".error-message");
+        var errorMessage2 = document.querySelector(".error-message2");
+        var ul = document.createElement("ul");
+
+        /*ustawiam flagę walidacji - do późniejszego wykorzystania*/
+        var ok = true;
+
+        // sprawdzenie tytulu (czy posiada więcej niż 1 i mniej niż 50 znaków)
+        if (title.value.length === 0) {
+            ok = false;
+            var msg = document.createElement("li");
+            msg.innerText = "Wprowadź tytuł";
+            ul.appendChild(msg);
         }
 
-        readLocalStorage();
+        if (title.value.length >= 50) {
+            ok = false;
+            var msg = document.createElement("li");
+            msg.innerText = "Tytuł jest za długi";
+            ul.appendChild(msg);
+        }
 
+        // sprawdzenie daty
+        if (date.value === "") {
+            ok = false;
+            var msg = document.createElement("li");
+            msg.innerText = "Uzupełnij datę";
+            ul.appendChild(msg);
+        }
 
-        console.log(localStorage.getItem('tasks'));
+        // sprawdzenie priorytetu
+        if (priority.value === "") {
+            ok = false;
+            var msg = document.createElement("li");
+            msg.innerText = "Nadaj priorytet";
+            ul.appendChild(msg);
+        }
 
-        /*nadaję event na button dodający zadanie*/
-        add.addEventListener("click", function() {
+        // sprawdzenie opisu (do 100 znaków)
+        if (description.value.length >= 100) {
+            ok = false;
+            var msg = document.createElement("li");
+            msg.innerText = "Opis jest za długi";
+            ul.appendChild(msg);
+        }
 
-            /*łapię wszystkie elementy*/
-            var title = document.querySelector("#title");
-            var date = document.querySelector("#date");
-            var priority = document.querySelector("#priority");
-            var description = document.querySelector("#description");
+        /* ---------------- LocalStorage -------------------- */
 
-            /* walidacja danych */
-            var errorMessage = document.querySelector(".error-message");
-            var errorMessage2 = document.querySelector(".error-message2");
-            var ul = document.createElement("ul");
+        function Todo(name) {
+            this.counter = counter;
+            this.title = title.value;
+            this.date = date.value;
+            this.priority = priority.value;
+            this.description = description.value;
+            this.completed = false;
+        }
 
-            /*ustawiam flagę walidacji - do późniejszego wykorzystania*/
-            var ok = true;
-
-            // sprawdzenie tytulu (czy posiada więcej niż 1 i mniej niż 50 znaków)
-            if (title.value.length === 0) {
-                ok = false;
-                var msg = document.createElement("li");
-                msg.innerText = "Wprowadź tytuł";
-                ul.appendChild(msg);
-            }
-
-            if (title.value.length >= 50) {
-                ok = false;
-                var msg = document.createElement("li");
-                msg.innerText = "Tytuł jest za długi";
-                ul.appendChild(msg);
-            }
-
-            // sprawdzenie daty
-            if (date.value === "") {
-                ok = false;
-                var msg = document.createElement("li");
-                msg.innerText = "Uzupełnij datę";
-                ul.appendChild(msg);
-            }
-
-            // sprawdzenie priorytetu
-            if (priority.value === "") {
-                ok = false;
-                var msg = document.createElement("li");
-                msg.innerText = "Nadaj priorytet";
-                ul.appendChild(msg);
-            }
-
-            // sprawdzenie opisu (do 100 znaków)
-            if (description.value.length >= 100) {
-                ok = false;
-                var msg = document.createElement("li");
-                msg.innerText = "Opis jest za długi";
-                ul.appendChild(msg);
-            }
-
-            /* ---------------- LocalStorage -------------------- */
-
-            function Todo(name) {
-                this.counter = counter;
-                this.title = title.value;
-                this.date = date.value;
-                this.priority = priority.value;
-                this.description = description.value;
-                this.completed = false;
-            }
-
-            // Add NewTodo
-            function addNewTodoWithName(name) {
-                var t = new Todo(name);
-                tasks.push(t);
-                counter++;
-                saveTasks();
-            }
-
-            // Get Todo
-
-            addNewTodoWithName(name);
-
-            // save data to local storage
-            function saveTasks() {
-                var str = JSON.stringify(tasks);
-                localStorage.setItem("tasks", str);
-            }
-
+        // Add NewTodo
+        function addNewTodoWithName(name) {
+            var t = new Todo(name);
+            tasks.push(t);
+            counter++;
             saveTasks();
-            var formObj = {
-                title: title.value,
-                date: date.value,
-                priority: priority.value,
-                description: description.value,
-            };
+        }
 
-            if (ok) {
-                // console.log("Walidacja okej");
-                errorMessage.innerHTML = "";
-                errorMessage2.innerHTML = "";
-                createElement(formObj);
+        // Get Todo
 
-                /*zeruję wartość inputu po dodaniu elementu*/
-                title.value = "";
-                date.value = "";
-                priority.value = "";
-                description.value = "";
-            } else {
-                errorMessage.innerHTML = "";
-                errorMessage.appendChild(ul);
+        addNewTodoWithName(name);
 
-                var mobile = window.matchMedia("(max-width: 480px)");
-                if (mobile.matches) {
-                    errorMessage2.innerHTML = "";
-                    errorMessage2.appendChild(ul);
-                }
-            }
+        // save data to local storage
+        function saveTasks() {
+            var str = JSON.stringify(tasks);
+            localStorage.setItem("tasks", str);
+        }
+
+        saveTasks();
+        var formObj = {
+            title: title.value,
+            date: date.value,
+            priority: priority.value,
+            description: description.value,
+        };
+
+        if (ok) {
+            // console.log("Walidacja okej");
+            errorMessage.innerHTML = "";
+            errorMessage2.innerHTML = "";
+            createElement(formObj);
 
             /*zeruję wartość inputu po dodaniu elementu*/
             title.value = "";
             date.value = "";
             priority.value = "";
             description.value = "";
-        });
+        } else {
+            errorMessage.innerHTML = "";
+            errorMessage.appendChild(ul);
 
-
-        /* ---- ukrywanie i chowanie elementu main-form ----- */
-
-        /*łapię formularz*/
-        var mainForm = document.querySelector(".main-form");
-
-        /*łapię przycisk +Add po wcześniejszym dodaniu mu ID mainAdd*/
-        var mainAdd = document.getElementById("mainAdd");
-
-        /*dodaję event, który chowa i wyświetla formularz*/
-
-        mainAdd.addEventListener("click", function () {
-            if (mainForm.style.display === "flex") {
-                mainForm.style.display = "none";
-            } else {
-                mainForm.style.display = "flex";
+            var mobile = window.matchMedia("(max-width: 480px)");
+            if (mobile.matches) {
+                errorMessage2.innerHTML = "";
+                errorMessage2.appendChild(ul);
             }
-        });
+        }
 
-        var arrowDate = document.querySelectorAll(".main-panel-change-direction")[0];
-        var arrowPriority = document.querySelectorAll(".main-panel-change-direction")[1];
-        var list = JSON.parse( localStorage.getItem('tasks') );
+        /*zeruję wartość inputu po dodaniu elementu*/
+        title.value = "";
+        date.value = "";
+        priority.value = "";
+        description.value = "";
+    });
 
-        console.log(arrowDate, arrowPriority, list);
 
-        arrowDate.addEventListener("click", function () {
-            var allLi = document.querySelectorAll(".new-task-li");
-            var allLiDown = document.querySelectorAll(".new-task-li-down");
-            var sorted = list.sort(compare);
+    /* ---- ukrywanie i chowanie elementu main-form ----- */
 
-            function compare(a,b) {
-                if (a.date < b.date)
-                    return -1;
-                if (a.date > b.date)
-                    return 1;
-                return 0;
-            }
+    /*łapię formularz*/
+    var mainForm = document.querySelector(".main-form");
 
-            for (var i=0; i<allLi.length; i++) {
-                taskList.removeChild(allLi[i]);
-                taskList.removeChild(allLiDown[i]);
-            }
+    /*łapię przycisk +Add po wcześniejszym dodaniu mu ID mainAdd*/
+    var mainAdd = document.getElementById("mainAdd");
 
-            for (var i = 0; i < list.length; i++) {
-                createElement(sorted[i]);
-            }
-        });
+    /*dodaję event, który chowa i wyświetla formularz*/
 
-        arrowPriority.addEventListener("click", function () {
+    mainAdd.addEventListener("click", function () {
+        if (mainForm.style.display === "flex") {
+            mainForm.style.display = "none";
+        } else {
+            mainForm.style.display = "flex";
+        }
+    });
 
-            var allLi = document.querySelectorAll(".new-task-li");
-            var allLiDown = document.querySelectorAll(".new-task-li-down");
-            var sorted = list.sort(compare);
+    /*pojawia sie opis*/
+    var x = document.querySelector(".main-tusks-list");
+    var y = document.querySelector(".new-task-li-down");
+    x.addEventListener("click", function () {
+        if (y.style.display === "flex") {
+            y.style.display = "none";
+        } else {
+            y.style.display = "flex";
+        }
+    });
 
-            function compare(a,b) {
-                if (a.priority < b.priority)
-                    return -1;
-                if (a.priority > b.priority)
-                    return 1;
-                return 0;
-            }
+    var arrowDate = document.querySelectorAll(".main-panel-change-direction")[0];
+    var arrowPriority = document.querySelectorAll(".main-panel-change-direction")[1];
+    var list = JSON.parse(localStorage.getItem('tasks'));
 
-            for (var i=0; i<allLi.length; i++) {
-                taskList.removeChild(allLi[i]);
-                taskList.removeChild(allLiDown[i]);
-            }
+    console.log(arrowDate, arrowPriority, list);
 
-            for (var i = 0; i < list.length; i++) {
-                createElement(sorted[i]);
-            }
+    arrowDate.addEventListener("click", function () {
+        var allLi = document.querySelectorAll(".new-task-li");
+        var allLiDown = document.querySelectorAll(".new-task-li-down");
+        var sorted = list.sort(compare);
 
-        });
+        function compare(a, b) {
+            if (a.date < b.date)
+                return -1;
+            if (a.date > b.date)
+                return 1;
+            return 0;
+        }
 
+        for (var i = 0; i < allLi.length; i++) {
+            taskList.removeChild(allLi[i]);
+            taskList.removeChild(allLiDown[i]);
+        }
+
+        for (var i = 0; i < list.length; i++) {
+            createElement(sorted[i]);
+        }
+    });
+
+    arrowPriority.addEventListener("click", function () {
+
+        var allLi = document.querySelectorAll(".new-task-li");
+        var allLiDown = document.querySelectorAll(".new-task-li-down");
+        var sorted = list.sort(compare);
+
+        function compare(a, b) {
+            if (a.priority < b.priority)
+                return -1;
+            if (a.priority > b.priority)
+                return 1;
+            return 0;
+        }
+
+        for (var i = 0; i < allLi.length; i++) {
+            taskList.removeChild(allLi[i]);
+            taskList.removeChild(allLiDown[i]);
+        }
+
+        for (var i = 0; i < list.length; i++) {
+            createElement(sorted[i]);
+        }
+
+    });
 
 
 });
